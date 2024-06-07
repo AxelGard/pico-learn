95897328b7b639069bdc5f976d96f60f4f1ff654
Axel Gard <axel.gard@protonmail.com>
2024-06-07 10:32:55 +0200

    added first bad version of svm

--- picolearn/svm.py
+++ picolearn/svm.py
@@ -6,13 +6,22 @@ class SVC:
 
     def __init__(self, kernal="linear") -> None:
         self.coef_: np.ndarray = None  # noted as w
+        self.intercept_ = 0.0
 
-    def fit(self, X: np.ndarray, y: np.ndarray):
-        b: np.ndarray = None
-        w: np.ndarray = None
-        w = b
+    def fit(self, X: np.ndarray, y: np.ndarray, epochs=1000, learning_rate=1e-2):
+        w = np.zeros(X.shape[1])
+        b = 0
+        for _ in range(epochs):
+            for i in range(len(X)):
+                x_i = X[i]
+                y_i = y[i]
+                if y_i * np.dot(x_i, w) + b < 1:
+                    w += learning_rate * y_i * x_i
+                    b += learning_rate * y_i
 
+        self.coef_ = np.array([w])
+        self.intercept_ = np.array([b])
         return self
 
     def predict(self, X: np.ndarray) -> np.ndarray:
-        pass
+        return np.sign(np.dot(X.T, self.coef_) + self.intercept_)
--- test/test_svm.py
+++ test/test_svm.py
@@ -1,24 +1,30 @@
 import numpy as np
-from sklearn.svm import SVC as SKLearnSVC
+from sklearn.svm import LinearSVC as SKLearnSVC
 from picolearn.svm import SVC as PicoLearnSVC
 
 
 def test_svc():
+
     X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
     y = np.array([1, 1, 2, 2])
     X_test = np.array([[-0.8, -1]])
 
-    sk_model = SKLearnSVC(kernel="linear")
+    sk_model = SKLearnSVC()
     sk_model.fit(X, y)
-
-    print(sk_model.coef_)
-
-    y_pred = sk_model.predict(X_test)
-    print(y_pred)
-
-    print("-" * 10)
 
     pl_model = PicoLearnSVC()
     pl_model.fit(X, y)
-    y_pred = pl_model.predict(X_test)
-    print(y_pred)
+
+    print("SKlearn:", sk_model.coef_, sk_model.intercept_)
+    print("Pico:", pl_model.coef_, pl_model.intercept_)
+
+    assert np.allclose(
+        sk_model.predict(X_test)[0], pl_model.predict(X_test)[0]
+    ), f"SVC predcit failed, should have been:{sk_model.predict(X_test)[0]} but was:{pl_model.predict(X_test)[0]}"
+    """assert np.allclose(
+        sk_model.coef_, pl_model.coef_
+    ), f"SVC coefficans failed, should have been:{sk_model.coef_} but was:{pl_model.coef_}"
+    assert np.allclose(
+        sk_model.intercept_, pl_model.intercept_
+    ), f"SVC intercept failed, should have been:{sk_model.intercept_} but was:{pl_model.intercept_}"
+    """
