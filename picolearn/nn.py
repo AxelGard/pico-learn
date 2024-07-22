4854d689171c7230c465ebfdab7baa39cf7964cd
Axel Gard <axel.gard@protonmail.com>
2024-07-22 16:19:05 +0200

    worked on MLP

--- picolearn/nn.py
+++ picolearn/nn.py
@@ -3,11 +3,23 @@ import numpy as np
 sigmoid = lambda x: 1 / (1 + np.e ** (-1 * x))
 relu = lambda x: [x * 0.01, x][int(x > 0)]
 
+sigmoid_derivative = lambda x: x * (1 - x)
 
-class NN:
-    """src: https://youtu.be/w8yWXqWQYmU?si=wuQvsusWI7YD3R3q"""
+
+class MLPClassifier:
 
     def __init__(
-        self, input_size: int, activation_func, number_of_labyers: int = 1
+        self,
+        max_iter: int = 300,
+        random_state: int = 123,
+        lr: float = 1e-3,
+        solver: str = "sgd",
     ) -> None:
-        pass
+        self.max_iter = max_iter
+        self.random_state = random_state
+        self.lr = lr
+        self.weights
+
+    def fit(self, X: np.ndarray, y: np.ndarray):
+
+        return self
--- test/test_nn.py
+++ test/test_nn.py
@@ -3,12 +3,21 @@ from sklearn.model_selection import train_test_split
 
 from sklearn.neural_network import MLPClassifier as sk_MLPClassifier
 from picolearn.nn import NN as pl_MLPClassifier
+from sklearn.metrics import accuracy_score
 
 
 def test_nn_mlp_clf():
 
     X, y = make_classification(n_samples=100, random_state=1)
-    X_train, X_test, y_train, _ = train_test_split(X, y, stratify=y, random_state=1)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, stratify=y, random_state=1
+    )
+    print(X)
 
     sk_clf = sk_MLPClassifier(random_state=1, max_iter=300).fit(X_train, y_train)
-    sk_clf.predict_proba(X_test[:1])
+    y_pred = sk_clf.predict(X_test)
+    print(y_pred)
+    print(accuracy_score(y_test, y_pred))
+
+
+# test_nn_mlp_clf()
